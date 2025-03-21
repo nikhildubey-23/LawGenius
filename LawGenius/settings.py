7420c24a07beb2c6f4750b871e17030f26b0f6f9
nikhildubey-23 <ravannick0089@gmail.com>
2025-03-21 12:34:18 +0530

    sending to another device

--- LawGenius/settings.py
+++ LawGenius/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', ]
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
